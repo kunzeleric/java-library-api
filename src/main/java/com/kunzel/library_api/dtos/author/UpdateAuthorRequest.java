a3b6dbe8347589bd3715a9f313d8a287114b828d
Eric Kunzel <kunzel.eric@gmail.com>
2026-07-20 20:04:02 -0300

    chore: adding @Pattern validation for update request DTOs

--- src/main/java/com/kunzel/library_api/dtos/author/UpdateAuthorRequest.java
+++ src/main/java/com/kunzel/library_api/dtos/author/UpdateAuthorRequest.java
@@ -3,9 +3,10 @@ package com.kunzel.library_api.dtos.author;
 import java.time.LocalDate;
 
 import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 
 public record UpdateAuthorRequest(
-        String name,
-        String nationality,
-        @Past(message = "deve ser uma data do passado") LocalDate birthDate) {
+                @Pattern(regexp = ".*\\S.*", message = "não deve estar em branco") String name,
+                @Pattern(regexp = ".*\\S.*", message = "não deve estar em branco") String nationality,
+                @Past(message = "deve ser uma data do passado") LocalDate birthDate) {
 }
--- src/main/java/com/kunzel/library_api/dtos/book/UpdateBookRequest.java
+++ src/main/java/com/kunzel/library_api/dtos/book/UpdateBookRequest.java
@@ -1,6 +1,11 @@
 package com.kunzel.library_api.dtos.book;
 
-public record UpdateBookRequest(String title, String isbn, Integer publishedYear,
-    String genre, Long authorId) {
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
+public record UpdateBookRequest(@Pattern(regexp = ".*\\S.*", message = "não deve estar em branco") String title,
+        @Pattern(regexp = ".*\\S.*", message = "não deve estar em branco") String isbn,
+        @Positive(message = "deve ser um valor positivo") Integer publishedYear,
+        @Pattern(regexp = ".*\\S.*", message = "não deve estar em branco") String genre, Long authorId) {
 
 }
